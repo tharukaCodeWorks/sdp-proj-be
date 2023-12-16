5da8e7a9088a63af159d75e2452c0e1142e242e4
aero-kwps <aero.kwps@gmail.com>
2023-12-16 17:19:36 +0530

    complain progress DAO created

---
+++ src/main/java/org/devsdp/wscms/complaint/dao/ComplaintProgressDao.java
@@ -0,0 +1,12 @@
+package org.devsdp.wscms.complaint.dao;
+
+import org.devsdp.wscms.complaint.model.ComplaintProgress;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author aerok
+ */
+public interface ComplaintProgressDao extends CrudRepository<ComplaintProgress, Integer> {
+
+}
