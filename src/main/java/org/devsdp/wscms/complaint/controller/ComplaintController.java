3ac2d1c9c1e151ac17cbb0b21122726ac4b9873c
aero-kwps <aero.kwps@gmail.com>
2023-12-17 18:20:19 +0530

    view-all and get-progress endpoints added

--- src/main/java/org/devsdp/wscms/complaint/controller/ComplaintController.java
+++ src/main/java/org/devsdp/wscms/complaint/controller/ComplaintController.java
@@ -1,5 +1,7 @@
 package org.devsdp.wscms.complaint.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.devsdp.wscms.sys.dto.ResponseWrapper;
 import org.devsdp.wscms.auth.model.User;
 import org.devsdp.wscms.complaint.dto.requests.ComplaintDto;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import org.devsdp.wscms.complaint.dto.responses.ComplaintProgressResDto;
+import org.devsdp.wscms.complaint.dto.responses.ComplaintViewResponseDto;
+import org.devsdp.wscms.complaint.model.ComplaintProgress;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -23,21 +29,61 @@ public class ComplaintController {
     private ComplaintService complaintService;
     private TokenProvider tokenProvider;
 
-    public ComplaintController(ComplaintService complaintService, TokenProvider tokenProvider){
+    public ComplaintController(ComplaintService complaintService, TokenProvider tokenProvider) {
         this.complaintService = complaintService;
         this.tokenProvider = tokenProvider;
     }
+
     @PostMapping(value = "/create")
     public ResponseEntity<ResponseWrapper> makeComplaint(@RequestBody ComplaintDto complaint, HttpServletRequest request) {
-        try{
+        try {
             String authorizationHeader = request.getHeader("Authorization");
             User user = tokenProvider.getAuthUser(request);
 
-            Complaint savedComplaint =  complaintService.makeComplaint(complaint, user.getId());
+            Complaint savedComplaint = complaintService.makeComplaint(complaint, user.getId());
             return ResponseEntity.ok(new ResponseWrapper<>(savedComplaint, "success", "success"));
 
-        }catch (Exception e) {
+        } catch (Exception e) {
             return ResponseEntity.ok(new ResponseWrapper<>(null, "failed", "Something went wrong! Please contact developer"));
         }
     }
+
+    // added by Prasad
+    @GetMapping(value = "/view-all")
+    public ResponseEntity<ResponseWrapper> viewComplaints(int userId,HttpServletRequest request) {
+//        User user = tokenProvider.getAuthUser(request);
+        List<Complaint> complaints = this.complaintService.getComplaints(1);
+        List<ComplaintViewResponseDto> resComplaintList = new ArrayList<>();
+        for (Complaint complaint : complaints) {
+            ComplaintViewResponseDto responseDto = new ComplaintViewResponseDto();
+            responseDto.setId(complaint.getId());
+            responseDto.setTitle(complaint.getComplaintTitle());
+            responseDto.setDescription(complaint.getComplaintDesc());
+            responseDto.setDivision(complaint.getDivision().getName());
+            responseDto.setReportedDate(complaint.getReportedDate());
+            responseDto.setStatus(complaint.getStatus());
+            resComplaintList.add(responseDto);
+        }
+        return ResponseEntity.ok(new ResponseWrapper<>(resComplaintList, "success", "success"));
+    }
+
+    @GetMapping(value = "/get-progress")
+    public ResponseEntity<ResponseWrapper> viewComplaintProgress(int complaintId) {
+        List<ComplaintProgress> complainProgressList = this.complaintService.getComplainProgressList(complaintId);
+        List<ComplaintProgressResDto> resCompProList = new ArrayList<>();
+        for (ComplaintProgress cp : complainProgressList) {
+            ComplaintProgressResDto cprd = new ComplaintProgressResDto();
+            cprd.setId(cp.getId());
+            cprd.setTitle(cp.getActionTitle());
+            cprd.setDescription(cp.getActionDesc());
+            cprd.setActionTypeId(cp.getActionType());
+            cprd.setActionTakenBy(cp.getActionTakenBy().getFirstName());
+            cprd.setComplaintId(complaintId);
+            cprd.setDate(cp.getRecordedDate());
+            resCompProList.add(cprd);
+        }
+        return ResponseEntity.ok(new ResponseWrapper<>(resCompProList, "success", "success"));
+
+    }
+
 }
